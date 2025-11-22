d4fff4966481cb3c5c095f95063c0dfa8947f75a
sonnytrader <snrfotograf@gmail.com>
2025-11-22 16:43:56 +0300

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,21 +1,16 @@
 const User = require('./User');
-const Subscription = require('./Subscription');
-const Payment = require('./Payment');
+const Signal = require('./Signal');
 const ApiKey = require('./ApiKey');
 
 // Associations
-User.hasMany(Subscription, { foreignKey: 'userId' });
-Subscription.belongsTo(User, { foreignKey: 'userId' });
-
-User.hasMany(Payment, { foreignKey: 'userId' });
-Payment.belongsTo(User, { foreignKey: 'userId' });
+User.hasMany(Signal, { foreignKey: 'userId' });
+Signal.belongsTo(User, { foreignKey: 'userId' });
 
 User.hasMany(ApiKey, { foreignKey: 'userId' });
 ApiKey.belongsTo(User, { foreignKey: 'userId' });
 
 module.exports = {
   User,
-  Subscription,
-  Payment,
+  Signal,
   ApiKey
 };
