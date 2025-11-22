840a3d202584f9f15aafc1859b36e6570e1a3a63
sonnytrader <snrfotograf@gmail.com>
2025-11-22 17:27:54 +0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const { User } = require('../models/User');
+const { user } = require('../models/user');
 
 const authenticateToken = async (req, res, next) => {
   const authHeader = req.headers['authorization'];
@@ -14,7 +14,7 @@ const authenticateToken = async (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findByPk(decoded.userId);
+    const user = await user.findByPk(decoded.userId);
     
     if (!user) {
       return res.status(401).json({
