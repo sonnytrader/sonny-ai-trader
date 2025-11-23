ead0a0fc1809d3774b22fdbd6123a8a0adca8cb6
sonnytrader <snrfotograf@gmail.com>
2025-11-24 02:31:45 +0300

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,15 @@
-const { Sequelize } = require('sequelize');
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite',
-  logging: false
+// Veritabanı dosyasının yolu
+const dbPath = path.resolve(__dirname, 'trade.db');
+
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+    console.error('Veritabanı bağlantı hatası:', err.message);
+  } else {
+    console.log('SQL Veritabanına bağlanıldı.');
+  }
 });
 
-module.exports = { sequelize };
+module.exports = db;
