4066a208096bbca31171cd6f729ebfa8c3bdbffa
sonnytrader <snrfotograf@gmail.com>
2025-11-23 13:41:53 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -5,7 +5,8 @@ const bcrypt = require('bcrypt');
 const User = sequelize.define('User', {
   username: { type: DataTypes.STRING, unique: true },
   password: { type: DataTypes.STRING },
-  subscription: { type: DataTypes.STRING, defaultValue: 'basic' }
+  subscription: { type: DataTypes.STRING, defaultValue: 'basic' },
+  role: { type: DataTypes.STRING, defaultValue: 'user' } // admin desteği
 });
 
 User.beforeCreate(async (user) => {
