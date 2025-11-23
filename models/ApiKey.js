052c682a7b0c028c0dc9e97a9b2d42930184513f
sonnytrader <snrfotograf@gmail.com>
2025-11-23 12:59:45 +0300

    Update ApiKey.js

--- models/ApiKey.js
+++ models/ApiKey.js
@@ -1,39 +1,9 @@
 const { DataTypes } = require('sequelize');
 const { sequelize } = require('../database');
-const crypto = require('crypto');
 
-const apikey = sequelize.define('apikey', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  userId: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  exchange: {
-    type: DataTypes.ENUM('bitget', 'binance'),
-    allowNull: false
-  },
-  apiKey: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  secret: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  passphrase: {
-    type: DataTypes.TEXT
-  },
-  isActive: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: true
-  },
-  label: {
-    type: DataTypes.STRING
-  }
+const ApiKey = sequelize.define('ApiKey', {
+  userId: { type: DataTypes.INTEGER },
+  key: { type: DataTypes.STRING }
 });
 
-module.exports = apikey;
+module.exports = ApiKey;
